d0d9ea19dc88fe839d307a4ea1fc49335839520d
apoorva1993 <baranwal.apoorva@gmail.com>
2026-05-09 20:47:16 +0530

    Initial commit of Playwright learning code

---
+++ chapter_01_Basics/02_JS.js
@@ -0,0 +1,11 @@
+let x = 10;
+console.log(x);
+
+for(let x = 0; x < 10000; x++) {
+    console.log(x);
+    print();
+}
+
+function print() {
+    console.log("Hello World");
+}
---
+++ chapter_01_Basics/03_Js_Commands.js
@@ -0,0 +1,11 @@
+console.log(process.platform);
+
+//MAC - DARWIN
+//WINDOWS - WIN32
+//LINUX - LINUX
+
+console.log(process.version);
+
+console.log(process.arch);
+
+console.log(process.cwd());
